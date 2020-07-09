95dff89c1a445e71545b26f12f0f51ac768504b3
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-08 21:41:33 -0300

    Testa TestTemperaturaMaxRepository - issue #29

--- estacao/tests/test_repositories.py
+++ estacao/tests/test_repositories.py
@@ -14,3 +14,14 @@ class TestTemperaturaMinRepository:
         temperatura_min = repository.get_temperatura_min('2017-12-31', '2018-02-01')
         expected = [(datetime(2018, 1, 1, 13, 48, 10), 12.0, 12.0)]
         assert expected == temperatura_min
+
+
+class TestTemperaturaMaxRepository:
+    def test_has_get_temperatura_max_attribute(self):
+        assert hasattr(TemperaturaRepository(), 'get_temperatura_max')
+
+    def test_temperatura_min(self, consolidado):
+        repository = TemperaturaRepository()
+        temepratura_max = repository.get_temperatura_max('2017-12-31', '2018-02-01')
+        expected = [(datetime(2018, 1, 1, 13, 48, 10), 50.0, 50.0)]
+        assert expected == temepratura_max
