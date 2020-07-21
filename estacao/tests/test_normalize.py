e5916ffcadf63d599fc7dae6ec1f0081508a77a6
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-21 09:28:45 -0300

    Testa trans_p - issue #31

--- estacao/tests/test_normalize.py
+++ estacao/tests/test_normalize.py
@@ -10,3 +10,12 @@ class TestNormalize:
         ep = normalize.ep(18)
         expected = 20.681888606861104
         assert ep == expected
+
+    def test_has_trans_p(self):
+        assert hasattr(Normalize(), 'trans_p')
+
+    def test_trans_p(self):
+        expected = 926.2597977212171
+        normalize = Normalize()
+        trans_p = normalize.trans_p(698.9, 25.0)
+        assert trans_p == expected
