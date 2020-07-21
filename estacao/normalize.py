0f2d6a0b084461483059d2ae18a625ac6f3e7358
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-21 09:31:16 -0300

    Adiciona rh_tw - issue #31

--- estacao/normalize.py
+++ estacao/normalize.py
@@ -11,3 +11,9 @@ class Normalize:
         p0 = p_mmhg * (1.0-0.000163 * tbar)
         p_hpa = (p0 - 1.3) * 1013.25/760.0
         return p_hpa
+
+    def rh_tw(self, dry, wet, p_hpa):
+        e = self.ep(wet) - p_hpa * (dry - wet) * 0.000653 * (1.0 + (0.000944 * wet))
+        es = self.ep(dry)
+        rh = (e / es) * 100
+        return rh
