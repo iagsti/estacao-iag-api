25151962faf928816349d5a9716782916bcc5b26
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-21 09:30:06 -0300

    Adiciona trans_p - issue #31

--- estacao/normalize.py
+++ estacao/normalize.py
@@ -6,3 +6,8 @@ class Normalize:
         exp = np.exp((17.62 * temperature)/(temperature + 243.12))
         resp = 1.0044 * 6.112 * exp
         return resp
+
+    def trans_p(self, p_mmhg, tbar):
+        p0 = p_mmhg * (1.0-0.000163 * tbar)
+        p_hpa = (p0 - 1.3) * 1013.25/760.0
+        return p_hpa
