5e2ea43971ef4825dc1e99298228bd3cbe9fe887
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-23 15:36:19 -0300

    Adiciona parâmetros start_date e end_date - issue #33
    Adiciona os parâmetros start_date e end_date na assinatura do método consolidado_factory,
    possibilitando definir ao período da data para o objeto Consolidado gerado.
    
    Os parâmetros não existiam antes, e o período da data do objeto gerado era fixa.

--- estacao/ext/factories.py
+++ estacao/ext/factories.py
@@ -1,8 +1,8 @@
 from estacao.ext import fakers
 
 
-def consolidado_factory(length):
+def consolidado_factory(length, start_date=None, end_date=None):
     data = list()
     for _ in range(length):
-        data.append(fakers.fake.consolidado())
+        data.append(fakers.fake.consolidado(start_date, end_date))
     return data
