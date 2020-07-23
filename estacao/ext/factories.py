995d5dda2b08a7be0499b727d833898b902ee711
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-23 15:41:31 -0300

    Adiciona factory para current_conditions - issue #33
    Adiciona factory para os dados das condições climátticas atuais.

--- estacao/ext/factories.py
+++ estacao/ext/factories.py
@@ -6,3 +6,7 @@ def consolidado_factory(length, start_date=None, end_date=None):
     for _ in range(length):
         data.append(fakers.fake.consolidado(start_date, end_date))
     return data
+
+
+def current_conditions_factory(length):
+    return [fakers.fake.current_conditions() for _ in range(length)]
