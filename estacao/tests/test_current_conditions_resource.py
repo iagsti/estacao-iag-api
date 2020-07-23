e656f7b3c027605101d0532679cc3faf6f8aee20
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-23 19:53:41 -0300

    Verifica se possui o atributo get - issue #33
    Verifica se CurrentConditionsResource possui o atributo get.

--- estacao/tests/test_current_conditions_resource.py
+++ estacao/tests/test_current_conditions_resource.py
@@ -1,6 +1,9 @@
+import json
 from estacao.blueprints.restapi.resources import CurrentConditionsResource
 from flask_restful import Resource
 from estacao.mixins.authentication_mixin import AuthMixin
+
+CURRENT_CONDITIONS_API = '/api/v0/current-conditions'
 
 
 class TestCurrentConditionsResource:
@@ -9,4 +12,7 @@ class TestCurrentConditionsResource:
 
     def test_has_authmixin(self):
         assert isinstance(CurrentConditionsResource(), AuthMixin)
+
+    def test_has_get_attribute(self):
+        assert hasattr(CurrentConditionsResource, 'get')
 
