759da34a5c1aca83817b5cf7557772a6acaafd57
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-23 19:42:51 -0300

    Testa instância de AuthMixin - issue #33
    Veirifica se CurrentConditionsResource herda a classe AuthMixin.

--- estacao/tests/test_current_conditions_resource.py
+++ estacao/tests/test_current_conditions_resource.py
@@ -1,7 +1,12 @@
 from estacao.blueprints.restapi.resources import CurrentConditionsResource
 from flask_restful import Resource
+from estacao.mixins.authentication_mixin import AuthMixin
 
 
 class TestCurrentConditionsResource:
     def test_is_resource_instance(self):
         assert isinstance(CurrentConditionsResource(), Resource)
+
+    def test_has_authmixin(self):
+        assert isinstance(CurrentConditionsResource(), AuthMixin)
+
