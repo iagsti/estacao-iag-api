9823fbf08860df90e41001926115fa9d2efd247e
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-23 16:00:30 -0300

    Testa o atributo model - issue #33
    Verifica se existe o atributo model e se este atributo é uma instância de Consolidado

--- estacao/tests/test_repositories.py
+++ estacao/tests/test_repositories.py
@@ -1,5 +1,7 @@
 from estacao.repositories import TemperaturaRepository
 from datetime import datetime
+from estacao.ext import factories
+from estacao.models import Consolidado
 
 
 class TestTemperaturaMinRepository:
@@ -25,3 +27,11 @@ class TestTemperaturaMaxRepository:
         temepratura_max = repository.get_temperatura_max('2017-12-31', '2018-02-01')
         expected = [(datetime(2018, 1, 1, 13, 48, 10), 50.0, 50.0)]
         assert expected == temepratura_max
+
+
+class TestCurrentConditionsRepository:
+    def test_has_model_attribute(self, current_conditions):
+        assert hasattr(current_conditions, 'model')
+
+    def test_model_instance(self, current_conditions, consolidado):
+        assert isinstance(current_conditions.model(), Consolidado)
