b2cb50d51778f61167a799428b74bb288611683a
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-24 23:24:31 -0300

    Gera factories com dados constatntes - issue #37
    Permite utilizar consolidado_factory gerando sempre os mesmos dados. Anteriormente os dados eram gerados de forma randômica para cada chamada.

--- estacao/ext/factories.py
+++ estacao/ext/factories.py
@@ -1,10 +1,10 @@
 from estacao.ext import fakers
 
 
-def consolidado_factory(length, start_date=None, end_date=None):
+def consolidado_factory(length, start_date=None, end_date=None, randomic=True):
     data = list()
     for _ in range(length):
-        data.append(fakers.fake.consolidado(start_date, end_date))
+        data.append(fakers.fake.consolidado(start_date, end_date, randomic))
     return data
 
 
