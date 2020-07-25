35b0a9c1103c06749de4eadbda30505e3e3e4638
Marcelo Schneider <schneider.fei@gmail.com>
2020-07-24 23:17:17 -0300

    Gera dados fixos para consolidado - issue #37
    Permite gerar dados constatnes para testes.
    Anteriormente os dados gerados eram randômicos.

--- estacao/ext/fakers.py
+++ estacao/ext/fakers.py
@@ -8,48 +8,41 @@ fake = Faker()
 
 
 class ConsolidadoProvider(BaseProvider):
-    def consolidado(self, start_date=None, end_date=None):
+    def consolidado(self, start_date=None, end_date=None, randomic=True):
+        float_elements = fake.random_element(elements=(12, 50))
+        dir_element = fake.random_element(elements=('Su', 'No', 'Le', 'O'))
+        if not randomic:
+            float_elements = 20.0
+            dir_element = 'Su'
         if not (start_date and end_date):
             start_date = datetime(2018, 1, 1, 13, 48, 10)
             end_date = datetime(2018, 1, 1, 13, 48, 10)
         obj = Consolidado(
             data=fake.date_time_between_dates(start_date, end_date),
-            vis=34,
-            tipob='tipob',
-            qtdb=fake.random_element(elements=(12, 50)),
-            tipom='tipom',
-            tipoa='tipoa',
-            qtda=fake.random_element(elements=(12, 50)),
-            dir=fake.random_element(elements=('Su', 'No', 'Le', 'O')),
-            vento=fake.random_element(elements=(12, 50)),
-            temp_bar=fake.random_element(elements=(12, 50)),
-            pressao=fake.random_element(elements=(12, 50)),
-            tseco=fake.random_element(elements=(12, 50)),
-            tumido=fake.random_element(elements=(12, 50)),
-            tsfc=fake.random_element(elements=(12, 50)),
-            t5cm=fake.random_element(elements=(12, 50)),
-            t10cm=fake.random_element(elements=(12, 50)),
-            t20cm=23,
-            t30cm=fake.random_element(elements=(12, 50)),
-            t40cm=fake.random_element(elements=(12, 50)),
-            piche=fake.random_element(elements=(12, 50)),
-            evap_piche=fake.random_element(elements=(12, 50)),
-            piche_ar=fake.random_element(elements=(12, 50)),
-            evap_piche_ar=fake.random_element(elements=(12, 50)),
-            tmin=fake.random_element(elements=(12, 50)),
-            tmax=fake.random_element(elements=(12, 50)))
+            vis=34, tipob='tipob', qtdb=float_elements, tipom='tipom',
+            tipoa='tipoa', qtda=float_elements, dir=dir_element,
+            vento=float_elements, temp_bar=float_elements,
+            pressao=float_elements, tseco=float_elements,
+            tumido=float_elements, tsfc=float_elements,
+            t5cm=float_elements, t10cm=float_elements, t20cm=23,
+            t30cm=float_elements, t40cm=float_elements, piche=float_elements,
+            evap_piche=float_elements, piche_ar=float_elements,
+            evap_piche_ar=float_elements, tmin=float_elements,
+            tmax=float_elements)
         return obj
 
 
 class CurrentConditionsProvider(BaseProvider):
-    def current_conditions(self):
-        data = datetime.now()
-        return dict(data=data, temperatura_ar=20.0,
-                    temperatura_ponto_orvalho=19,
-                    umidade_relativa=90, temperatura_min=18,
-                    temperatura_max=23, visibilidade=10,
-                    vento=30, pressao=160, nuvens_baixas='Cum/20',
-                    nuvens_medias='Nim/20', nuvens_altas='Cir/30')
+    def current_conditions(self, **kwargs):
+        now = datetime.now()
+        default = dict(data=str(now.date()), temperatura_ar=20.0,
+                       temperatura_ponto_orvalho=19,
+                       umidade_relativa=90, temperatura_min=18,
+                       temperatura_max=23, visibilidade=10,
+                       vento=30, pressao=160, nuvens_baixas='Cum/20',
+                       nuvens_medias='Nim/20', nuvens_altas='Cir/30')
+        data = dict(default, **kwargs)
+        return data
 
 
 providers = (
